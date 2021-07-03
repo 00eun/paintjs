19588fe61d40f2e45c20cb9d79f9fef8e856ebb4
youngeun <00eun1128@gmail.com>
2021-07-03 13:02:34 +0900

    이미지 다운로드 기능 추가

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const ctx = canvas.getContext("2d");
 const color = document.querySelectorAll(".jsColor");
 const range = document.querySelector(".controls__range input");
 const mode = document.querySelector("#jsMode");
+const saveBtn = document.querySelector("#jsSave");
 
 let painting = false;
 let filling = false;
+const DEFAULTCOLOR = "#2c2c2c";
 
 canvas.width = document.querySelector("canvas").offsetWidth;
 canvas.height = document.querySelector("canvas").offsetHeight;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.lineWidth = 2.5;
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = DEFAULTCOLOR;
+ctx.fillStyle = DEFAULTCOLOR;
 
 function onMouseMove(event) {
   const x = event.offsetX;
@@ -38,6 +43,7 @@ if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 const colors = Array.from(color);
@@ -47,6 +53,7 @@ function handleColorClick(event) {
   const changeClick = event.target.style.backgroundColor;
   console.log(changeClick);
   ctx.strokeStyle = changeClick;
+  ctx.fillStyle = changeClick;
 }
 
 colors.forEach((color) => color.addEventListener("click", handleColorClick));
@@ -66,10 +73,29 @@ function handleModeClick() {
   }
 }
 
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+function handleSaveImg() {
+  const dataURL = canvas.toDataURL();
+  console.log(dataURL);
+  const link = document.createElement("a");
+  link.href = dataURL;
+  link.download = "PaintJS[🎁]";
+  link.click();
+}
+
 if (range) {
   range.addEventListener("input", handleChangeRange);
 }
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveImg);
 }
